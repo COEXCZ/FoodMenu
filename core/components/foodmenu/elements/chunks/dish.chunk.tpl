e5629f8ea5c20c9d3ee5b6174c0f19e3e009495b
Jan Tezner <jan.tezner@gmail.com>
2013-03-22 19:55:35 +0100

    Snippet for dishes listing

--- core/components/foodmenu/elements/chunks/dish.chunk.tpl
+++ core/components/foodmenu/elements/chunks/dish.chunk.tpl
@@ -1,1 +1,9 @@
-<li><strong>[[+name]]</strong> - [[+description]]</li>
+<li>
+    [[+tip:is=`1`:then=`<span class="tip">TIP ŠÉFKUCHAŘE</span>`]]
+    <h3 class="[[+tip:is=`1`:then=`tip`]]">[[+name]]</h3>
+    [[+description:isnot=``:then=`<p>[[+description]]</p>`]]
+    <p>
+        [[+weight:isnot=`Váha: [[+weight]]<br />`]]
+        Cena: <strong class="price">[[+price]]</strong> Kč
+    </p>
+</li>
--- core/components/foodmenu/elements/snippets/FoodMenu.snippet.php
+++ core/components/foodmenu/elements/snippets/FoodMenu.snippet.php
@@ -4,38 +4,64 @@
  *
  * @package foodmenu
  */
-$FoodMenu = $modx->getService('foodmenu','FoodMenu',$modx->getOption('foodmenu.core_path',null,$modx->getOption('core_path').'components/foodmenu/').'model/foodmenu/',$scriptProperties);
+$FoodMenu = $modx->getService('foodmenu', 'FoodMenu', $modx->getOption('foodmenu.core_path', null, $modx->getOption('core_path') . 'components/foodmenu/') . 'model/foodmenu/', $scriptProperties);
 if (!($FoodMenu instanceof FoodMenu)) return '';
 
-/**
- * Do your snippet code here. This demo grabs 5 items from our custom table.
- */
-$tpl = $modx->getOption('tpl',$scriptProperties,'Item');
-$sortBy = $modx->getOption('sortBy',$scriptProperties,'name');
-$sortDir = $modx->getOption('sortDir',$scriptProperties,'ASC');
-$limit = $modx->getOption('limit',$scriptProperties,5);
-$outputSeparator = $modx->getOption('outputSeparator',$scriptProperties,"\n");
+$output = array();
+$output_dishes = array();
 
-/* build query */
-$c = $modx->newQuery('FoodMenuItem');
-$c->sortby($sortBy,$sortDir);
-$c->limit($limit);
-$items = $modx->getCollection('FoodMenuItem',$c);
+// set variables
+$tpl = $modx->getOption('tpl', $scriptProperties, 'Category');
+$itemTpl = $modx->getOption('itemTpl', $scriptProperties, 'Dish');
 
-/* iterate through items */
-$list = array();
-foreach ($items as $item) {
-    $itemArray = $item->toArray();
-    $list[] = $FoodMenu->getChunk($tpl,$itemArray);
+$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, "\n");
+$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
+
+// get categories
+$c = $modx->newQuery('FoodMenuCategory');
+$c->sortBy('position', 'asc');
+
+$categories = $modx->getCollection('FoodMenuCategory', $c);
+
+foreach ($categories as $cat) {
+    $cat = $cat->toArray();
+
+    // get dishes for current category
+    $c = $modx->newQuery('FoodMenuDish');
+    $c->where(array(
+        'category' => $cat['id']
+    ));
+    $c->sortBy('position', 'asc');
+
+    $dishes = $modx->getCollection('FoodMenuDish', $c);
+
+    // skip category if no dishes
+    if (count($dishes) < 1) {
+        continue;
+    }
+
+    // iterate over all dishes in category
+    foreach ($dishes as $dish) {
+        $dish = $dish->toArray();
+
+        // render dish
+        $output_dishes[] = $FoodMenu->getChunk($itemTpl, $dish);
+    }
+
+    // extend category for dishes render
+    $cat['dishes'] = implode($outputSeparator, $output_dishes);
+
+    // render category with dishes
+    $output[] = $FoodMenu->getChunk($tpl, $cat);
 }
 
-/* output */
-$output = implode($outputSeparator,$list);
-$toPlaceholder = $modx->getOption('toPlaceholder',$scriptProperties,false);
+// output
+$output = implode($outputSeparator, $output);
 if (!empty($toPlaceholder)) {
-    /* if using a placeholder, output nothing and set output to specified placeholder */
-    $modx->setPlaceholder($toPlaceholder,$output);
+    // if using a placeholder, output nothing and set output to specified placeholder
+    $modx->setPlaceholder($toPlaceholder, $output);
     return '';
 }
-/* by default just return output */
+
+// by default just return output
 return $output;
